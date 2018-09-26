8a3126049e4e64dee23d7f198bd8b13d9efc6248
sean <huangcrab@gmail.com>
2018-09-25 23:06:26 -0400

    demo shell added

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@ $(window).load(function() {
     .delay(1000)
     .fadeOut();
 });
-
+let state = "main";
 $(document).ready(function() {
   onload();
+  // $("#demos").click(function() {
+  //   //window.history.pushState("object or string", "Title", "/demos");
+  //   $(".header-overlay").fadeOut();
+  //   $(".header-overlay-about").fadeOut();
+  //   $(".header-content").hide("slide", { direction: "right" }, 1500);
+  //   $(".header-content-about").hide("slide", { direction: "right" }, 1500);
+  //   $(".fullscreen-video-wrap video")
+  //     .fadeOut(1500)
+  //     .delay(500)
+  //     .queue(function(next) {
+  //       $(this).attr("src", "assets/demo.mp4");
+  //       next();
+  //     })
+  //     .fadeIn(1000);
+  // });
 
   $("#about").click(function() {
     $(".header-content").hide();
@@ -16,27 +31,61 @@ $(document).ready(function() {
     $(".header-content").fadeIn();
   });
 
-  $("#projects,#projects-about").click(function() {
+  $("#projects,#projects-about,#demos,#demos-about").click(function() {
+    const demo =
+      $(this)
+        .attr("id")
+        .indexOf("demo") !== -1;
+    const dir = demo ? "right" : "left";
     $(".header-overlay").fadeOut();
     $(".header-overlay-about").fadeOut();
-    $(".header-content").hide("slide", { direction: "left" }, 1500);
-    $(".header-content-about").hide("slide", { direction: "left" }, 1500);
-    $(".fullscreen-video-wrap video")
-      .fadeOut(1500)
-      .delay(500)
-      .queue(function(next) {
-        $(this).attr("src", "assets/project.mp4");
-        next();
-      })
-      .fadeIn(1000);
-    $(".projects").delay(1500);
-    $(".projects").fadeIn(2000);
+    $(".header-content").hide("slide", { direction: dir }, 1500);
+    $(".header-content-about").hide("slide", { direction: dir }, 1500);
+
+    if (demo) {
+      state = "demo";
+      $(".fullscreen-video-wrap video")
+        .fadeOut(1500)
+        .delay(500)
+        .queue(function(next) {
+          $(this).attr("src", "assets/demo.mp4");
+          next();
+        })
+        .fadeIn(1000);
+      $(".demos").delay(1500);
+      $(".demos").fadeIn(2000);
+    } else {
+      state = "project";
+      $(".fullscreen-video-wrap video")
+        .fadeOut(1500)
+        .delay(500)
+        .queue(function(next) {
+          $(this).attr("src", "assets/project.mp4");
+          next();
+        })
+        .fadeIn(1000);
+      $(".projects").delay(1500);
+      $(".projects").fadeIn(2000);
+    }
   });
 
-  $("#close").click(function() {
-    $(".projects").fadeOut(500);
-    $(".header-content").delay(500);
-    $(".projects").hide();
+  $("#close,#close-demo").click(function() {
+    state = "main";
+    assignActive("main");
+    const demo =
+      $(this)
+        .attr("id")
+        .indexOf("demo") !== -1;
+    const dir = demo ? "right" : "left";
+    if (demo) {
+      $(".demos").fadeOut(500);
+      $(".header-content").delay(500);
+      $(".demos").hide();
+    } else {
+      $(".projects").fadeOut(500);
+      $(".header-content").delay(500);
+      $(".projects").hide();
+    }
     $(".header-overlay").fadeIn(1500);
     $(".fullscreen-video-wrap video")
       .fadeOut(1500)
@@ -47,28 +96,35 @@ $(document).ready(function() {
       })
       .delay(500)
       .fadeIn(500);
-    $(".header-content").show("slide", { direction: "right" }, 1500);
+    $(".header-content").show("slide", { direction: dir }, 1500);
   });
 
-  $("#next").click(function() {
-    moveDown();
+  $(".control-next").click(function(e) {
+    moveDown(e);
   });
-  jQuery(window).on("swipeleft", function() {
-    moveUp();
+  jQuery(window).on("swipeleft", function(e) {
+    moveUp(e);
   });
-  $("#prev").click(function() {
-    moveUp();
+  $(".control-prev").click(function(e) {
+    moveUp(e);
   });
-  jQuery(window).on("swiperight", function() {
-    moveDown();
+  jQuery(window).on("swiperight", function(e) {
+    moveDown(e);
   });
 });
-const projects = document.querySelectorAll(".project");
-const array = [];
 
-function moveDown() {
+const indicator = state === "demo" ? "demo" : "project";
+const projects = document.querySelectorAll(`.${indicator}`);
+
+[...document.querySelectorAll(".sum-project")].forEach(element => {
+  element.innerHTML = projects.length;
+});
+
+function moveDown(e) {
+  console.log(indicator);
   let nextKey;
-  const num = $(".active").attr("id");
+  const cName = e.currentTarget.id.indexOf("demo") !== -1 ? "demo" : "project";
+  const num = $(`.${cName}.active`).attr("id");
   $.each(projects, function(key, value) {
     if (num == value.id) {
       nextKey = key + 1;
@@ -96,9 +152,10 @@ function moveDown() {
   $("#prev").fadeIn();
 }
 
-function moveUp() {
+function moveUp(e) {
   let nextKey;
-  const num = $(".active").attr("id");
+  const cName = e.currentTarget.id.indexOf("demo") !== -1 ? "demo" : "project";
+  const num = $(`.${cName}.active`).attr("id");
   $.each(projects, function(key, value) {
     if (num == value.id) {
       nextKey = key - 1;
@@ -128,5 +185,16 @@ function moveUp() {
 
 function onload() {
   $(".projects").hide();
+  $(".demos").hide();
   $(".header-content-about").hide();
+}
+
+function assignActive(state) {
+  // if (state === "demo") {
+  //   document.querySelector(".demo").className += " active";
+  // } else if (state === "project") {
+  //   document.querySelector(".project").className += " active";
+  // } else {
+  //   document.querySelector(".active").classList.remove("active");
+  // }
 }
